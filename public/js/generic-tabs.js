ee83bd416ee987a57be8bc511c1bd6567c8f0553
ifch0o1 <ifch0o69@gmail.com>
2015-04-29 15:34:08 +0300

    initial commit

---
+++ public/js/generic-tabs.js
@@ -0,0 +1,53 @@
+var genTabs = (function() {
+	var tabs = $('.generic-tab');
+	var contentHolder = $('.tab-content');
+	var templates = {
+		knowledge: 'templates/tab-content/knowledge.html',
+		work: 'templates/tab-content/work.html',
+		motivation: 'templates/tab-content/motivation.html',
+		contact: 'templates/tab-content/contact.html'
+	};
+
+	var content = (function() {
+		function load(path) {
+			// contentHolder.empty();
+			contentHolder.load(path);
+		}
+
+		return {
+			load: load
+		}
+	}());
+
+	var controller = (function() {
+		function activeTab(tab) {
+			var $tab = $(tab);
+			$('.generic-tab').removeClass('active');
+			$tab.addClass('active');
+		}
+
+		$('.tab-knowledge').on('click', function() {
+			content.load(templates.knowledge);
+			activeTab($(this));
+		});
+		$('.tab-work').on('click', function() {
+			content.load(templates.work);
+			activeTab($(this));
+		});
+		$('.tab-motiv').on('click', function() {
+			content.load(templates.motivation);
+			activeTab($(this));
+		});
+		$('.tab-contact').on('click', function() {
+			content.load(templates.contact);
+			activeTab($(this));
+		});
+	}());
+
+	// Load default content
+	$('.tab-knowledge').trigger('click');
+
+	return {
+		list: tabs
+	}
+}());
---
+++ public/js/startup-animations.js
@@ -0,0 +1,60 @@
+// 'use strict';
+
+$(document).ready(function() {
+	var icons = {
+		portfolio: 'images/icons/port.png',
+		knowledge: 'images/icons/know.png',
+		doc: 'images/icons/doc.png',
+		project: 'images/icons/proj.png',
+		idea: 'images/icons/idea.png'
+	};
+
+	var w = $(this).width(),
+	    h = $(this).height();
+
+	function sideAnimation($img, $wrapper, orientation, offset, options) {
+		if (orientation !== 'left' && orientation !== 'right') {
+			throw new Error('Expect orientation argument as string \'left\' or \'right\'');
+		}
+		if (!($img instanceof $) || !($wrapper instanceof $)) {
+			throw new TypeError('Expect $img and $wrapper arguments as jQuery objects');
+		}
+		$img.css({
+			position: 'absolute',
+			// left: (orientation === 'left') ? -50 - $img.width() : w + $img.width() + 50
+			left: (orientation === 'left') ? -50 - $img.width() : 'auto',
+			right: (orientation === 'right') ? -50 - $img.width() : 'auto'
+		});
+		$wrapper.css('position', 'relative').append($img);
+		$img.animate({
+			// left: (orientation === 'left') ? offset : w - offset
+			left: (orientation === 'left') ? offset : 'auto',
+			right: (orientation === 'right') ? offset : 'auto'
+		}, options);
+	}
+
+	function isScrolledIntoView(elem) {
+	    var $elem = $(elem);
+	    var $window = $(window);
+
+	    var docViewTop = $window.scrollTop();
+	    var docViewBottom = docViewTop + $window.height();
+
+	    var elemTop = $elem.offset().top;
+	    var elemBottom = elemTop + $elem.height();
+
+	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	}
+
+	$('<img src="' + icons.portfolio + '">').load(function() {
+		var $that = $(this);
+		$that.css('top', '25%');
+
+		setTimeout(function() {
+			sideAnimation($that, $('#header'), 'right', '25%', {
+				duration: 800,
+				easing: 'easeOutCubic'
+			});
+		}, 1200);
+	});
+});
